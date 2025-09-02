a006c319f7177854debddc376a24fd432d4f5acf
saranzafar <saran.development@gmail.com>
2025-09-02 17:58:34 +0500

    updated

--- ch46-ckEditor/blog/urls.py
+++ ch46-ckEditor/blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
-from blog.views import home
+from blog.views import home, blog_list, blog_detail
 
 urlpatterns = [
     path('', home, name='home'),
+    path("blogs/", blog_list, name="blog_list"),
+    path("blogs/<slug:slug>/", blog_detail, name="blog_detail"),
 ]
--- ch46-ckEditor/blog/views.py
+++ ch46-ckEditor/blog/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from blog.forms import CreatePostForm
 from blog.models import Post
 
+
 def home(request):
-    print("i called")
+    # Handle form submission
     if request.method == "POST":
         form = CreatePostForm(request.POST, request.FILES)
         if form.is_valid():
@@ -12,5 +13,14 @@ def home(request):
     else:
         form = CreatePostForm()
 
-    blog_posts = Post.objects.all().order_by('-created_at')
-    return render(request, 'blog/home.html', {'form': form, 'blog_posts': blog_posts})
+    return render(request, 'blog/home.html', {'form': form})
+
+
+def blog_list(request):
+    blogs = Post.objects.all().order_by('-created_at')
+    return render(request, "blog/blog_list.html", {"blogs": blogs})
+
+
+def blog_detail(request, slug):
+    blog = get_object_or_404(Post, slug=slug)
+    return render(request, "blog/blog_detail.html", {"blog": blog})
