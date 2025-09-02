aadad141b4e35eb06d843ffd0e7c042dcd47071c
Usman Arif <usmn2391@gmail.com>
2025-09-02 16:55:24 +0500

    Fix CKEditor5 'invalid form control is not focusable' bug

--- ch46-ckEditor/blog/forms.py
+++ ch46-ckEditor/blog/forms.py
@@ -3,6 +3,10 @@ from blog.models import Post
 from django_ckeditor_5.widgets import CKEditor5Widget
 
 class CreatePostForm(forms.ModelForm):
+    content = forms.CharField(
+        widget=CKEditor5Widget(config_name='extends'),
+        required=False
+    )
     class Meta:
         model = Post
         fields = ['title', 'short_description', 'content', 'featured_image']
