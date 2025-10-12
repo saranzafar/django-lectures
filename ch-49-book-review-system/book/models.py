ed1e74aa0774d8c137dea09b96e10550ea2a45e3
saranzafar <saran.development@gmail.com>
2025-10-12 15:31:18 +0500

    book review system

--- ch-49-book-review-system/book/models.py
+++ ch-49-book-review-system/book/models.py
@@ -19,18 +19,12 @@ class Book(models.Model):
     genre = models.CharField(max_length=20, choices=GENRE_CHOICES)
     isbn = models.CharField('ISBN', max_length=13, unique=True)
     publication_date = models.DateField()
-    average_rating = models.DecimalField(
-        max_digits=3,
-        decimal_places=2,
-        validators=[MinValueValidator(0), MaxValueValidator(5)],
-        default=Decimal('0.00')
-    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
     
-    def get_absolute_url(self):
-        return reverse("bookdetail", kwargs={"pk": self.pk})
+    # def get_absolute_url(self):
+    #     return reverse("bookdetail", kwargs={"pk": self.pk})
     
--- ch-49-book-review-system/book/views.py
+++ ch-49-book-review-system/book/views.py
@@ -15,3 +15,35 @@ def book_create(request):
     else:
         form = BookForm()
     return render(request, 'book/book_form.html', {'form': form})
+
+def book_list(request):
+    genre = request.GET.get('genre')
+    if genre:
+        books = Book.objects.filter(genre=genre).order_by('created_at')
+    else:
+        books = Book.objects.all().order_by('created_at')
+    return render(request, 'book/book_list.html', {'books': books})
+
+def book_detail(request, pk):
+    book = Book.objects.get(pk=pk)
+    return render(request, 'book/book_detail.html', {'book': book})
+
+def book_update(request, pk):
+    book = Book.objects.get(pk=pk)
+    if request.method == "POST":
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Book updated successfully!')
+            return redirect('book-details', pk=book.pk)
+        else:
+            messages.error(request, 'All fields are required.')
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'book/book_form.html', {'form': form})
+
+def book_delete(request, pk):
+    book = Book.objects.get(pk=pk)
+    book.delete()
+    messages.success(request, 'Book deleted successfully!')
+    return redirect('book-list')
